b51ce8553e068ddd8cf4a5261d79b8f5535add32
oxuyingqio <oxuyingqio@qq.com>
2017-07-11 18:58:26 +0800

    x

--- src/main/resources/web/core/project/search/Search.js
+++ src/main/resources/web/core/project/search/Search.js
@@ -19,8 +19,9 @@ core.project.search.Search = (function() {
 	function dealEasyUI(search, tr, config) {
 
 		// 添加标签单元格
-		tr.append(new core.html.element.viewer.Td().append(new core.html.element.viewer.Label().append(config.label
-				+ ":")));
+		tr.append(new core.html.element.viewer.Td().colspan(config.colspan === undefined ? 1 : config.colspan).rowspan(
+				config.rowspan === undefined ? 1 : config.rowspan).append(
+				new core.html.element.viewer.Label().append(config.label + ":")));
 
 		// 添加搜索框单元格
 		var td = new core.html.element.viewer.Td().style("word-break:keep-all; white-space:nowrap;");
